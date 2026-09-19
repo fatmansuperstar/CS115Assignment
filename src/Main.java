00d743e8d5d1ea8348ac8a0c69bad92f2bf423a2
Ben Nutt <bennutt9@gmail.com>
2026-09-19 08:01:01 -0700

    Final assignment v1

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,26 @@ import java.util.InputMismatchException;
 public class Main {
     public static void main(String[] args) throws Exception {
 
+        //Declares the variables that will be used to store user information
+        double[] userQuizScores = new double[10];
+        double avgQuizScores = 0;
+        char letterGrade;
+
         getSpecifications(); //Calls the getSpecifications method
 
-        enterScores();
+        //Calls the enterScores() method and saves it in the variable userQuizScores
+        userQuizScores = enterScores();
+        System.out.println("The quiz scores you entered were " + Arrays.toString(userQuizScores));
+
+        //Calls the calculateAvgQuizScore() method and passes userQuizScores as the argument. Saves it to the variable avgQuizScores.
+        avgQuizScores = calculateAvgQuizScore(userQuizScores);
+        System.out.println("Average quiz score is equal to " + avgQuizScores + "%");
+
+        //Calls the calculateLetterGrade() method and passes avgQuizScores as the argument. Saves letter grade in variable letterGrade.
+        letterGrade = calculateLetterGrade(avgQuizScores);
+        System.out.println("You letter grade: " + letterGrade);
+
+
 
         
     }
@@ -19,32 +36,82 @@ public class Main {
     }
 
     public static double[] enterScores(){
-       double exitLoop = 0;
+       double exitLoop = 0; //This variable will be used to test whether or not the loop for entering scores should be exited
        Scanner scanner = new Scanner(System.in);
-       double userInput;
-       double[] grades = new double[10];
+       double userInput; //Variable to store what the user enters
+       double[] grades = new double[10]; //Creates an array of 10 buckets 
        
        
-       while(exitLoop != 999){
-            for (int i = 1; i <=10; i++){
+       while(exitLoop != 999){ //While the user does not enter 999, keep asking for quiz scores
+            for (int i = 1; i <=10;){ //For loop to ask for a maximum of 10 quiz socres
                 try{
                 System.out.print("Enter a quiz grade: ");
-                userInput = scanner.nextDouble();
-                exitLoop = userInput;
-                grades[i-1] = userInput;
-                if(i == 10 || exitLoop == 999){
-                    exitLoop = 999;
+                userInput = scanner.nextDouble(); //Store what the user enters
+                 exitLoop = userInput; //Store what the user enters in the exitLoop variable
+                if(i == 10 || exitLoop == 999){ //If 10 scores have been entered or the user entered 999
+                    exitLoop = 999; //Breaks out of the while loop
                     System.out.println("All grades entered.");
-                    break;
+                    break; //Breaks out of the for loop
                 }
-            }catch(InputMismatchException e){
+                else if(userInput < 0){ //If user enters a grade less than 0%
+                    System.out.println("You can not have a negative quiz score. Please try again");
+                    scanner.nextLine(); //Clear their answer and ask again
+                }
+                else{ //If the score they entered is valid
+                i++; //Increment the number of quiz that they are entering
+                grades[i-1] = userInput; //save the quiz score in the grades Array
+                }
+                
+            }catch(InputMismatchException e){ //Catches the error of the user not entering a number
                 System.out.println("You must enter a valid quiz score.");
-                scanner.nextLine();
+                scanner.nextLine(); //Clear their answer and ask again
             }
             }
        }
+       
+       return grades; //Returns their array of grades
+    }
 
-       System.out.println(Arrays.toString(grades));
-       return grades;
+    public static double calculateAvgQuizScore(double[] quizScores){
+        //local variables declared and initialized
+        double averageQuizScore = 0;
+        double totalQuizScore = 0;
+        int quizCount = 0;
+
+        for(int i = 0; i < quizScores.length; i++){ //For each quiz score
+            totalQuizScore += quizScores[i]; //Add the quiz scores up
+            if(quizScores[i] != 0){ //Only add quiz attempts for the number of quizzes they actually took
+                quizCount++;
+            }
+        }
+
+        averageQuizScore = totalQuizScore/quizCount; //calculate average quiz score
+
+        
+        return averageQuizScore; //return average quiz score
+    }
+
+    public static char calculateLetterGrade(double averageQuizScore){
+        //Declare and initialize letterGrade
+        char letterGrade = 'F';
+
+        //If statements granting a letter grade depending on what the average score is
+        if(averageQuizScore > 89 && averageQuizScore <= 100){
+            letterGrade = 'A';
+        }
+        else if(averageQuizScore > 79 && averageQuizScore <= 89){
+            letterGrade = 'B';
+        }
+        else if (averageQuizScore > 69 && averageQuizScore <= 79){
+            letterGrade = 'C';
+        }
+        else if (averageQuizScore > 59 && averageQuizScore <= 69){
+            letterGrade = 'D';
+        }
+        else{
+            letterGrade = 'F';
+        }
+
+        return letterGrade; //return letter grade
     }
 }
